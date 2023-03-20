d696a8b68594bc22ce9f137ddf4cc4d885aac3d0
Kevin <kevintu34@gmail.com>
2023-03-20 11:26:04 -0700

    working version of server

---
+++ src/dishes/dishes.router.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+const dishesController = require("./dishes.controller")
+const methodNotAllowed = require("../errors/methodNotAllowed")
+
+// TODO: Implement the /dishes routes needed to make the tests pass
+
+router.route("/:dishId")
+    .get(dishesController.read)
+    .put(dishesController.update)
+    .all(methodNotAllowed)
+
+router.route("/")
+    .get(dishesController.list)
+    .post(dishesController.create)
+    .all(methodNotAllowed)
+
+module.exports = router;
